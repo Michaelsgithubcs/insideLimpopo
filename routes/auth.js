348253c6cc5efd4c7db750c4129720556062332f
lehlogonolo-hitek <jimmy.aphane@hiteknologysolutions.co.za>
2025-03-28 04:23:00 +0200

    Login && Register Routes( );

--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 // Login Router
 router.get('/login', (req, res) => {
-    res.render('login_register', { Login: true});
+    res.render('login_register', { title : 'Login', login: true});
 });
 
 // Register Router
 router.get('/register', (req, res) => {
-    res.render('login_register', { register: true});
+    res.render('login_register', {title: 'Register', register: true});
 });
 
 module.exports = router;
